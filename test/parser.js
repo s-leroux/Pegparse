453fc0224ddd825a9d4d0fc994e26ab41d0d7b38
Sylvain Leroux <sylvain@chicoree.fr>
2021-02-18 14:28:09 +0100

    Add more tests for litterals

--- test/parser.js
+++ test/parser.js
@@ -6,15 +6,39 @@ const assert = require("chai").assert;
 const g = require("../lib/grammar.js");
 
 describe("parser", function() {
-  it("should start and terminate on empty code", function() {
+  it("should start and end on empty grammar", function() {
     const grammar = new g.Grammar();
     grammar.define("r1", []);
 
-    const parser = grammar.parser("r1", g.litteral("a"));
+    const parser = grammar.parser("r1");
     parser.accept("abc");
 
     assert.equal(parser.status, "success");
     assert.equal(parser.running, false);
+  });
+
+  it("should match litterals (cuccess)", function() {
+    const grammar = new g.Grammar();
+    grammar.define("r1", g.litteral("a"));
+
+    const parser = grammar.parser("r1");
+    parser.accept("abc");
+
+    assert.equal(parser.status, "success");
+    assert.equal(parser.running, false);
+    assert.equal(parser.tx, 1);
+  });
+
+  it("should match litterals (failure)", function() {
+    const grammar = new g.Grammar();
+    grammar.define("r1", g.litteral("a"));
+
+    const parser = grammar.parser("r1");
+    parser.accept("bc");
+
+    assert.equal(parser.status, "failure");
+    assert.equal(parser.running, false);
+    assert.equal(parser.tx, 0);
   });
 });
 
